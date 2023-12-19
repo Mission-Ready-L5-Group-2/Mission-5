26f0f8be896a02747ec068e77082357c52b2c59d
kriraw14 <krishan.rawat@outlook.com>
2023-12-20 01:14:05 +1300

    Moved footer to shared component

--- frontend/src/pages/krish/SavedProperties.jsx
+++ frontend/src/pages/krish/SavedProperties.jsx
@@ -33,29 +33,7 @@ function SavedProperties() {
           <p>${property.price} per week</p>
           <button>Add notes</button>
         </div>
-      ))}
-
-      <footer style={{display: 'flex', justifyContent: 'space-between'}}>
-        <div>
-          <img src="logo.png" alt="Logo" />
-          <div>
-            <a href="https://www.facebook.com">Facebook</a>
-            <a href="https://www.instagram.com">Instagram</a>
-            <a href="https://www.linkedin.com">LinkedIn</a>
-            <a href="https://www.twitter.com">Twitter</a>
-          </div>
-        </div>
-        <div>
-          <p>Phone: 09039104642</p>
-          <p>Email: info@metronz.co.nz</p>
-          <p>Address: Level 33, ANZ Centre, 23-29 Albert Street, Auckland CBD 1010</p>
-        </div>
-      </footer>
-      <div style={{textAlign: 'center'}}>
-        © 2023 Metro NZ Property Management
-        <br />
-        SEO Services Provided By KWD - An SEO Auckland NZ Company
-      </div>
+      ))}      
     </div>
   );
 }
--- frontend/src/sharedcomponents/RootLayout.jsx
+++ frontend/src/sharedcomponents/RootLayout.jsx
@@ -4,8 +4,28 @@ function RootLayout() {
   return (
     <>
       <nav>Create Navbar, how does this work</nav>
-      <Outlet />
-      <footer>Create Footer, no sir!</footer>
+      <Outlet />      
+      <footer style={{display: 'flex', justifyContent: 'space-between'}}>
+        <div>
+          <img src="logo.png" alt="Logo" />
+          <div>
+            <a href="https://www.facebook.com">Facebook</a>
+            <a href="https://www.instagram.com">Instagram</a>
+            <a href="https://www.linkedin.com">LinkedIn</a>
+            <a href="https://www.twitter.com">Twitter</a>
+          </div>
+        </div>
+        <div>
+          <p>Phone: 09039104642</p>
+          <p>Email: info@metronz.co.nz</p>
+          <p>Address: Level 33, ANZ Centre, 23-29 Albert Street, Auckland CBD 1010</p>
+        </div>
+        <div style={{textAlign: 'center'}}>
+        © 2023 Metro NZ Property Management
+        <br />
+        SEO Services Provided By KWD - An SEO Auckland NZ Company
+      </div>
+      </footer>
     </>
   );
 }
